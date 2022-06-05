83908ae432f3383453cc8f84b6333c0c99abbd92
Gelin Eguinosa <eguinosa@gmail.com>
2022-06-05 13:45:06 -0400

    Test RandomSample() class.

--- random_sample.py
+++ random_sample.py
@@ -1,11 +1,12 @@
 # Gelin Eguinosa Rosique
 
 import json
+import random
 from os.path import join, isdir, isfile
-from random import sample
 
 from papers_analyzer import PapersAnalyzer
-from extra_funcs import progress_bar
+from extra_funcs import progress_bar, big_number
+from time_keeper import TimeKeeper
 
 
 class RandomSample:
@@ -18,7 +19,7 @@ class RandomSample:
     sample_index_file = 'random_sample_index.json'
     sample_index_prefix = 'random_sample_index_'
 
-    def __init__(self, paper_type='all', size=-1, use_saved=False,
+    def __init__(self, paper_type='all', sample_size=-1, use_saved=False,
                  sample_id=None, show_progress=False):
         """
         Create a Random Sample of documents from the CORD-19 dataset, or load a
@@ -27,7 +28,7 @@ class RandomSample:
         Args:
             show_progress: Bool representing whether we show the progress of
                 the function or not.
-            size: An int with size of the sample. The default value '-1'
+            sample_size: An int with size of the sample. The default value '-1'
                 represents all the papers available with the specified paper
                 type.
             use_saved: A Bool indicating if we are loading the sample from a
@@ -63,17 +64,17 @@ class RandomSample:
                 progress_bar(1, 1)
         else:
             # Create a new Random Sample.
-            self.sample_index = self._new_random_sample(paper_type=paper_type,
-                                                        size=size)
+            self.sample_index = self._new_random_sample(paper_type=paper_type, sample_size=sample_size)
             # Progress
             if show_progress:
                 print("Creating new Random Sample...")
                 progress_bar(1, 1)
             # Save the new Sample Index.
-            with open(self.sample_index_file, 'w') as f:
+            self.sample_index_path = join(self.data_folder, self.sample_index_file)
+            with open(self.sample_index_path, 'w') as f:
                 json.dump(self.sample_index, f)
 
-    def _new_random_sample(self, paper_type='all', size=-1):
+    def _new_random_sample(self, paper_type='all', sample_size=-1):
         """
         Create a random sample of papers from the CORD-19 dataset, with the
         specified paper type and sample size.
@@ -81,7 +82,7 @@ class RandomSample:
         Args:
             paper_type: A string with the type of papers we want in the sample.
                 They can be 'small', 'medium', 'big' or 'all' types.
-            size: An int with size of the sample. The default value '-1'
+            sample_size: An int with size of the sample. The default value '-1'
                 represents all the papers available with the specified paper
                 type.
         Returns:
@@ -100,16 +101,16 @@ class RandomSample:
             raise NameError("The type of papers is not specified.")
 
         # Get the Sample Size:
-        if size < 0:
+        if sample_size < 0:
             total = len(papers)
         else:
-            total = min(size, len(papers))
+            total = min(sample_size, len(papers))
 
         # Create the Random Sample.
-        random_sample = sample(papers, total)
+        random_sample = random.sample(papers, total)
         return random_sample
 
-    def sample_titles_abtracts(self):
+    def sample_titles_abstracts(self):
         """
         Create an iterator with the titles and abstracts of the sample's papers.
 
@@ -155,7 +156,7 @@ class RandomSample:
             sample_id: A string or number as the Sample ID.
         """
         # Create Random Sample path.
-        sample_index_file = self.sample_index_prefix + sample_id + '.json'
+        sample_index_file = self.sample_index_prefix + str(sample_id) + '.json'
         sample_index_path = join(self.data_folder, sample_index_file)
         # Save Sample Index in file.
         with open(sample_index_path, 'w') as f:
@@ -179,3 +180,47 @@ class RandomSample:
             sample_index_path = join(cls.data_folder, cls.sample_index_file)
         result = isdir(cls.data_folder) & isfile(sample_index_path)
         return result
+
+
+if __name__ == '__main__':
+    # Record the Runtime of the Program
+    stopwatch = TimeKeeper()
+
+    # Test the class.
+    test_size = 3_000
+    print(f"\nCreating a Random Sample of {big_number(test_size)} documents...")
+    sample = RandomSample('medium', test_size, show_progress=True)
+    print("Done.")
+    print(f"[{stopwatch.formatted_runtime()}]")
+
+    print("\nFirst 5 Document of the sample:")
+    count = 0
+    for doc_id in sample.sample_index[:5]:
+        count += 1
+        print(f"Document {count} - cord_uid: {doc_id}")
+
+    print(f"\nSaving Random Sample and creating a New One...")
+    sample.save_sample('01')
+    sample = RandomSample('medium', test_size)
+    print("Done.")
+    print(f"[{stopwatch.formatted_runtime()}]")
+
+    print("\nLoading old Random Sample:...")
+    old_sample = RandomSample(use_saved=True, sample_id='01')
+    print("Done.")
+    print(f"[{stopwatch.formatted_runtime()}]")
+
+    print("\nFirst 5 Document of the New Sample:")
+    count = 0
+    for doc_id in sample.sample_index[:5]:
+        count += 1
+        print(f"Document {count} - cord_uid: {doc_id}")
+
+    print("\nFirst 5 Document of the Old Sample:")
+    count = 0
+    for doc_id in old_sample.sample_index[:5]:
+        count += 1
+        print(f"Document {count} - cord_uid: {doc_id}")
+
+    print("\nDone.")
+    print(f"[{stopwatch.formatted_runtime()}]\n")
