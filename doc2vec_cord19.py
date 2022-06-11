dd162d4a81d093c5e1c6f331441288960e5be197
Gelin Eguinosa <eguinosa@gmail.com>
2022-06-11 15:39:49 -0400

    Add Doc2Vec model using the CORD-19 dataset.

---
+++ doc2vec_cord19.py
@@ -0,0 +1,114 @@
+# Gelin Eguinosa Rosique
+
+from gensim.models import doc2vec
+from gensim.utils import simple_preprocess
+
+from random_sample import RandomSample
+from time_keeper import TimeKeeper
+
+
+class Doc2VecCord19:
+    """
+    Create a Topic Model of the CORD-19 dataset using Doc2Vec.
+    """
+    # Class Data Locations.
+    data_folder = 'project_data'
+    doc2vec_model_file = 'doc2vec_cord19_model'
+    doc2vec_index = 'doc2vec_index.json'
+
+    def __init__(self, vector_size=300, paper_type='all', corpus_size=-1,
+                 use_saved=False, show_progress=False):
+        """
+        Create a Doc2Vec and Word2Vec model using the CORD-19 dataset.
+
+        Args:
+            paper_type: The type of Papers we want to use for the Sample (small,
+                medium or big).
+            corpus_size: An int with size of the sample. The default value '-1'
+                represents all the papers available with the specified paper
+                type.
+            use_saved: A Bool indicating if we are loading the sample from a
+                file.
+            show_progress: A Bool representing whether we show the progress of
+                the function or not.
+        """
+        # Load CORD-19 documents.
+        self.doc2vec_corpus = RandomSample(paper_type=paper_type,
+                                           sample_size=corpus_size,
+                                           show_progress=show_progress)
+        # Tokenize the documents and Tag them.
+        train_corpus = _TaggedCorpus(self.doc2vec_corpus.docs_titles_abstracts)
+
+        # Build & Train the Model.
+        self.doc2vec_model = doc2vec.Doc2Vec(vector_size=vector_size,
+                                             min_count=2,
+                                             epochs=40)
+        self.doc2vec_model.build_vocab(train_corpus)
+        self.doc2vec_model.train(train_corpus,
+                                 total_examples=self.doc2vec_model.corpus_count,
+                                 epochs=self.doc2vec_model.epochs)
+        # Get the Word2Vec model.
+        self.word2vec_model = self.doc2vec_model.wv
+
+        # Save class Attributes.
+
+
+class _TaggedCorpus:
+    """
+    Iterable Class used to provide the tokenized tagged documents when training
+    the Doc2Vec model.
+    """
+    def __init__(self, docs_generator_func):
+        self.docs_generator_func = docs_generator_func
+
+    def __iter__(self):
+        return doc2vec_preprocess(self.docs_generator_func())
+
+
+def doc2vec_preprocess(docs):
+    """
+    Preprocess the corpus documents, tokenize the texts into individual words,
+    remove punctuation, set to lower case, and remove words to long or too short.
+
+    Args:
+        docs: A list of strings - The text of the documents.
+    Returns:
+        An iterator of Tagged Documents (containing processed tokens).
+    """
+    # Use the position of the docs as the Tag.
+    for tag, doc in enumerate(docs):
+        # Use the Gensim Tokenizer
+        doc_tokens = simple_preprocess(doc, deacc=True)
+        yield doc2vec.TaggedDocument(doc_tokens, [tag])
+
+
+if __name__ == '__main__':
+    # Record the Runtime of the Program
+    stopwatch = TimeKeeper()
+
+    # Test the class.
+    print("\nCreating Doc2Vec model...")
+    test_size = 500
+    doc_model = Doc2VecCord19(vector_size=50, paper_type='medium',
+                              corpus_size=test_size, show_progress=True)
+    print("Done.")
+    print(f"[{stopwatch.formatted_runtime()}]")
+
+    # Test Word Vector.
+    print("\nWord vector of 'the':")
+    print(doc_model.word2vec_model['the'])
+
+    print("\nShow Part of the vocabulary:")
+    for index, word in enumerate(doc_model.word2vec_model.index_to_key):
+        if index == 10:
+            break
+        print(f"word #{index}/{len(doc_model.word2vec_model.index_to_key)} is {word}")
+
+    test_word = 'the'
+    print(f"\nShow most similar words to {test_word}:")
+    similar_words = doc_model.word2vec_model.most_similar('patient', topn=15)
+    for word in similar_words:
+        print(f" -> {word}")
+
+    print("\nDone.")
+    print(f"[{stopwatch.formatted_runtime()}]\n")
